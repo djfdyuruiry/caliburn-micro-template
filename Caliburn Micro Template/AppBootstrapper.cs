78a814cfae85ac237d5bddd02034f0d2939938b8
djfdyuruiry <djfdyuruiry@hotmail.com>
2017-03-14 18:38:15 +0000

    Inital Template Project

---
+++ Caliburn Micro Template/AppBootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+using Caliburn.Micro;
+using System;
+
+namespace CaliburnMicroTemplate
+{
+    public class AppBootstrapper : BootstrapperBase
+    {
+        private readonly AppContainer _appContainer;
+
+        public AppBootstrapper()
+        {
+            _appContainer = new AppContainer();
+
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e) => DisplayRootViewFor<ShellViewModel>();
+
+        protected override void Configure() => _appContainer.Configure();
+
+        protected override IEnumerable<object> GetAllInstances(Type service) => _appContainer.GetAllInstances(service);
+
+        protected override object GetInstance(System.Type service, string key) => _appContainer.GetInstance(service);
+    }
+}
---
+++ Caliburn Micro Template/AppContainer.cs
@@ -0,0 +1,16 @@
+﻿using Caliburn.Micro;
+using SimpleInjector;
+
+namespace CaliburnMicroTemplate
+{
+    public class AppContainer : Container
+    {
+        public void Configure()
+        {
+            Register<IWindowManager, WindowManager>();
+            RegisterCollection<ShellView>();
+
+            Verify();
+        }
+    }
+}
